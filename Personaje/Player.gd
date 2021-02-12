5aa17572e8da01629bf2fe7007561be05fcb6586
JuanROV99 <71752715+JuanROV99@users.noreply.github.com>
2021-02-12 00:09:46 -0800

    12/12/2021
    incluye stage_1

--- Personaje/Player.gd
+++ Personaje/Player.gd
@@ -4,34 +4,64 @@ const ground = Vector2(0,-1)
 var on_ground = false
 const gravity = 50
 const maxfallspeed = 800
-const maxspeed = 600
+const maxspeed = 500
 const jumpspeed = -1200
-
 var motion = Vector2()
 
+export var firedelay = 0.2
+
+var kunai = preload("res://Personaje/kunai.tscn")
 func _ready():
 	pass
 
 func _physics_process(_delta):
 	if Input.is_action_pressed("right"):
 		motion.x = maxspeed
-		$AnimatedSprite.play("run")
+		#$AnimatedSprite.play("run")
+		$AnimatedSprite.play("doctor_run")
 		$AnimatedSprite.flip_h = false
+		
+		if sign($Position2D.position.x) == -1:
+			$Position2D.position.x *=-1
 		
 	elif Input.is_action_pressed("left"):
 		motion.x = -maxspeed    
-		$AnimatedSprite.play("run") 
+		#$AnimatedSprite.play("run")
+		$AnimatedSprite.play("doctor_run") 
 		$AnimatedSprite.flip_h = true
+		
+		if sign($Position2D.position.x) == 1:
+			$Position2D.position.x *=-1
 	else:
 		motion.x = 0
 		if on_ground == true:
-			$AnimatedSprite.play("idle")
+			#if Input.is_action_pressed("trigger") == false:
+			#$AnimatedSprite.play("idle")
+			$AnimatedSprite.play("doctor_idle")
 		
 		
 	if Input.is_action_pressed("jump"):
 		if on_ground == true:
 			motion.y = jumpspeed
 			on_ground = false
+	
+	if Input.is_action_pressed("trigger") and $cooldown.is_stopped():
+		$cooldown.start(firedelay)
+		var kn = kunai.instance()
+		if sign($Position2D.position.x) == 1:
+			kn.set_kn_direction(1)
+		else:
+			kn.set_kn_direction(-1)
+		get_parent().add_child(kn)
+		kn.position = $Position2D.global_position
+			
+		if on_ground == true and motion.x == 0: 
+
+			$AnimatedSprite.play("doctor_idle")
+			
+		elif on_ground == true and motion.x != 0: 
+			$AnimatedSprite.play("doctor_run")
+	
 				
 	motion.y += gravity
 	if motion.y > maxfallspeed:
@@ -45,6 +75,7 @@ func _physics_process(_delta):
 			$AnimatedSprite.play("jump")
 		else:
 			$AnimatedSprite.play("fall")
+			
 			
 # warning-ignore:return_value_discarded
 	move_and_slide(motion, ground)
--- Personaje/kunai.gd
+++ Personaje/kunai.gd
@@ -1,13 +1,26 @@
 extends Area2D
 
-const speed = 100
-var motion = Vector2(100,0)
+const speed = 1000
+var motion = Vector2()
+var direction = 1
 
 func _ready():
 	pass # Replace with function body.
 
+func set_kn_direction(dir):
+	direction = dir
+	if dir == -1:
+		$AnimatedSprite.flip_h = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physis_process(delta):
-	motion.x = speed*delta
-	translate(Vector2(100,0))
+func _physics_process(delta):
+	motion.x = speed*delta * direction
+	translate(motion)
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+func _on_kunai_body_entered(body):
+	queue_free()
