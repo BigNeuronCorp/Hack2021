4f4a2ea2718f2b77ce319757edadfa8a6d5a3874
JuanROV99 <71752715+JuanROV99@users.noreply.github.com>
2021-02-14 05:38:30 -0800

    Update playing_ui.gd

--- ui/playing_ui.gd
+++ ui/playing_ui.gd
@@ -1,29 +1,30 @@
 extends Node2D
 
 func _on_right_pressed():
-	Input.action_press("ui_right")
+	Input.is_action_just_pressed("ui_right")
 func _on_right_released():
-	Input.action_release("ui_right")
+	Input.action_just_release("ui_right")
 
 
 func _on_left_pressed():
-	Input.action_press("ui_left")
+	Input.action_just_press("ui_left")
 func _on_left_released():
-	Input.action_release("ui_left")
+	Input.action_just_release("ui_left")
 
 
-func _on_jump_pressed():
+func _on_jump_just_pressed():
 	Input.action_press("ui_select")
-func _on_jump_released():
+func _on_jump_just_released():
 	Input.action_release("ui_select")
 
 
-func _on_shoot_pressed():
+func _on_shoot_just_pressed():
 	Input.action_press("ui_focus_next")
-func _on_shoot_released():
+func _on_shoot_just_released():
 	Input.action_release("ui_focus_next")
 
-func _on_pause_pressed():
+
+func _on_pause_just_pressed():
 	Input.action_press("ui_cancel")
-func _on_pause_released():
+func _on_pause_just_released():
 	Input.action_release("ui_cancel")
